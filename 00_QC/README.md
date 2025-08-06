5c7fcd5146a9641e05f3e415e372a9d3d187f7fb
Nitin Vincent <nitiowo98@gmail.com>
2025-08-06 13:59:16 -0400

    Adding fastqc and multiqc scripts

---
+++ 00_QC/README.md
@@ -0,0 +1,2 @@
+## This directory is for performing initial exploratory quality checks on all the files using fastqc.
+# Run fastqc and multiqc on all files and look at mulitqc output
---
+++ 00_QC/multiqc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --login
+
+# Load multiqc (eg; HPC module or conda) if necessary
+#conda activate fastqc-env
+
+# Creat output directory if it doesn't exist
+mkdir -p ./multiqc_out
+
+# Run multiqc on fastqc output
+multiqc ./fastqc_out/ -o ./multiqc_out
+
+# Deactivate multiqc
+#conda deactivate if necessary
