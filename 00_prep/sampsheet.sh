edd0dc7238e2f2d748a2f76a6b4f8eb526d7f1e5
Nitin Vincent <nitiowo98@gmail.com>
2025-08-14 17:56:21 -0400

    Changing output directory

--- 00_prep/sampsheet.sh
+++ 00_prep/sampsheet.sh
@@ -3,4 +3,4 @@
 # This script generates a sample sheet from your raw data directory
 
 cd ../raw_data
-ls -1 *R1*.fastq.gz | sed -E 's/_R1.*$//' | sort -u > samples.txt
+ls -1 *R1*.fastq.gz | sed -E 's/_R1.*$//' | sort -u > ../00_prep/samples.txt
