214c1d71fc3e08564642857b26241fb9a7bf48f1
Nitin Vincent <nvincen2@bastion.crc.nd.edu>
2025-08-19 02:55:02 -0400

    fixing rawdir

--- 00_prep/sampsheet.sh
+++ 00_prep/sampsheet.sh
@@ -2,9 +2,9 @@
 
 # This script generates a sample sheet from your raw data directory
 
-# Usage: bash sampsheet.sh <raw_directory>
+# Usage: bash sampsheet.sh
 
-RAW_DIR=$1
+RAW_DIR=$(cat raw_dir.txt)
 
 cd $RAW_DIR
 ls -1 *R1*.fastq.gz | sed -E 's/_R1.*$//' | sort -u > ../00_prep/samples.txt
