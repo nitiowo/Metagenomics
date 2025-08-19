5194b125148fb4c7021bf55c9b536d725b789fbc
Nitin Vincent <nvincen2@bastion.crc.nd.edu>
2025-08-19 02:49:54 -0400

    Adding rawdir to sampsheet.sh

--- 00_prep/sampsheet.sh
+++ 00_prep/sampsheet.sh
@@ -2,5 +2,9 @@
 
 # This script generates a sample sheet from your raw data directory
 
-cd ../raw_data
+# Usage: bash sampsheet.sh <raw_directory>
+
+RAW_DIR=$1
+
+cd $RAW_DIR
 ls -1 *R1*.fastq.gz | sed -E 's/_R1.*$//' | sort -u > ../00_prep/samples.txt
