e90856843174dabcc5ea617753628c3c522c105d
Nitin Vincent <nitiowo98@gmail.com>
2025-09-18 17:26:34 -0400

    test

---
+++ 01_QC/QC_scripts/fastQC.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+
+# Load fastqc (eg; HPC module or conda) if necessary
+# conda activate fastqc-env
+
+# Create output directory if it doesn't exist
+mkdir -p ../fastqc_out
+
+# Finding raw data
+RAW_DIR=$(cat ../../00_prep/raw_dir.txt )
+
+# Run fastqc on every file in raw data directory
+fastqc "$RAW_DIR"/* -o ../fastqc_out/
+
+# Deactivate fastqc
+# conda deactivate if necessary
