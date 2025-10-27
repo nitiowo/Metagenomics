4d95839a1ba28e6fef45ca068a7cc57249f19bfa
Nitin Vincent <nitiowo98@gmail.com>
2025-10-27 16:31:29 -0400

    tax_distribution script done

--- 06_phyloseq/plot_scripts/tax_distribution.R
+++ 06_phyloseq/plot_scripts/tax_distribution.R
@@ -138,7 +138,6 @@ random_tree <- rtree(ntaxa(orig_ps), rooted=TRUE,
 ps <- merge_phyloseq(orig_ps, random_tree) # Add tree to ps object
 
 
-
 ####################################################################
 
 # Taxonomic distribution
@@ -147,21 +146,87 @@ ps <- merge_phyloseq(orig_ps, random_tree) # Add tree to ps object
 
 tax_plot <- function(ps, top_x, group_variables,levels,
                      marker_outdir, marker, tax_subset){
+  
+  # Removing problem samples
+  if (marker == "Folmer"){
+    ps2 <- subset_samples(ps, !Tube %in% c("68","9"))
+  } else if (marker == "18S"){
+    ps2 <- subset_samples(ps, Tube != "57")
+  } else if (marker == "Leray" || marker == "Morphological"){
+    ps2 <- ps
+  }
+  
+  # Iterating through tax subsets
+  ##################################################################
   for(tax_subset in tax_subsets){
+    
+    if(tax_subset == "noRotifer"){
+      # Subsetting out Rotifers
+      ps <- subset_taxa(ps2, Phylum != "Rotifera")
+    } else if(tax_subset == "onlyRotifer"){
+      # Subsetting only Rotifers
+      ps <- subset_taxa(ps2, Phylum == "Rotifera")
+    } else if(tax_subset == "noBenthic"){
+      # Subsetting samples with Morphological data
+      ps <- subset_samples(ps2, Morph_avail == "YES")
+    } else if(tax_subset == "onlyBenthic"){
+      # Subsetting samples without Morphological data
+      ps <- subset_samples(ps2, Morph_avail == "NO")
+    } else if(tax_subset == "noRotifer_noBenthic"){
+      # Subsetting out samples with no Morphological data and Rotifers
+      ps <- ps2 %>% 
+        subset_taxa(Phylum != "Rotifera") %>% 
+        subset_samples(Morph_avail == "YES")
+    } else if(tax_subset == "unfiltered"){
+      ps <- ps2
+    }
+    
+    # Iterating through top_x levels
+    ##################################################################
     for(top in top_x){
+      
+      # Transform counts and prune top samples
+      if(top != "all"){
+        topx <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:top]
+      } else if(top == "all"){
+        topx <- names(sort(taxa_sums(ps), decreasing=TRUE))
+      }
+      
+      ps.transform <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
+      ps.topx <- prune_taxa(topx, ps.transform)
+      
+      topchar <- as.character(top)
+      
+      # Iterating through tax ranks
+      ##################################################################
       for(level in levels){
+        
+        # Glom by level (use NArm = FALSE to keep NAs). Don't glom if level=ASV
+        if(level != "ASV"){
+          topx_glom <- tax_glom(ps.topx, taxrank = level)
+          plotfill <- level
+        } else if(level == "ASV"){
+          topx_glom <- ps.topx
+          plotfill <- "Species"
+        }
+        
+        # Iterating through group facet vars
+        ##################################################################
         for(group_var in group_variables){
+          
+          x_var <- "Sample" # Set x axis variable
           
           if(group_var == "Morph_avail" && grepl("Benthic", tax_subset)){
             next
           } else if (marker == "Morphological" && group_var != "Lake"){
             next
+          } else if (group_var == "Mesh"){
+            x_var <- "Station_ID"  # Make x axis Station ID if faceting by Mesh
           }
           
-          topchar <- as.character(top)
+          print(paste("Plotting tax distribution, Top", top, level, group_var, sep = " "))
           
-          print(paste("Plotting top", top, level, group_var, sep = " "))
-          
+          # Creating output filepath
           outfilepath <- paste0(marker_outdir, "/", tax_subset, "/")
           
           outfilepath1 <- paste0(outfilepath, level, "/top", topchar, "/")
@@ -170,39 +235,29 @@ tax_plot <- function(ps, top_x, group_variables,levels,
           outfilebase <- paste0(outfilepath1, marker, "_", level, "_", tax_subset)
           outfilename <- paste0(outfilebase, "_", level, "_top", top, "_by", group_var,".png")
           
+          # Create plot title
+          title_marker <- paste("(Marker:", marker, ")")
+          title_rank <- paste("(Taxrank:", level, ")")
+          title_subset <- paste("(Subset:", tax_subset, ")")
+          title_topx <- paste("(Topx:", "Top ", topchar, " taxa)")
+          title_facet <- ifelse(group_var == "all", "", paste("by ", group_var))
+          title <- paste("Taxonomic distribution", title_marker, title_rank, title_subset,
+                         title_topx, title_facet, sep = " " )
           
-          title <- paste0(marker, " Taxonomic distribution of top ", top, ' ', 
-                          level, " by ", group_var, " - ", tax_subset)
-          # Plot bar chart of top 20 abundant ASVs
-          topx <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
-          ps.transform <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
-          ps.topx <- prune_taxa(topx, ps.transform)
-          
-          topx_glom <- tax_glom(ps.topx, taxrank = level)
-          
-          bar.p <- plot_bar(topx_glom, fill=level, x="Station_ID") + 
-            #x = "Station_ID") +
-            # theme_bw() +
+          # Create bar plot
+          bar.p <- plot_bar(topx_glom, fill=plotfill, x=x_var) + 
+            # If group_var is not "all", facet by group_var
             {if(group_var != "all")facet_wrap(vars(.data[[group_var]]), scales="free_x")} +
-          #facet_wrap(~Lake) + 
-          ggtitle(title)  +
-            theme(plot.title = element_text(size = 25, hjust = 0.5), 
+            ggtitle(title)  +
+            theme(plot.title = element_text(size = 12.5, hjust = 0.5), 
                   axis.text.x = element_text(angle = 90)) +
             ylab("Relative abundance") 
-          # geom_bar(stat="identity", position = "stack")
           
-          # scale_fill_discrete(breaks = c("Huron", "Michigan", "Superior", "Erie", "Ontario")) # change order of legend
-          show(bar.p)
-          return(bar.p)
+          # Save plot
+          ggsave(filename = outfilename, plot = bar.p,
+               width = 1000, height = 800, device = png, units = "px", scale = 3)
           
-          # plot_bar(ps.transform, fill="Order") +
-          #   facet_wrap(~Lake, scales = "free_x") +
-          #   ylab("Relative abundance") + 
-          #   ggtitle("18S - Overall biodiversity of Great Lakes at Order level from all sequences")
-          
-          # ggsave using last_plot() to save whatever versions you want. Make sure to change names
-          # ggsave(filename = paste0(outdir, "/morph_station_top20_wt.png"), plot = last_plot(), 
-          #        width = 1000, height = 800, device = png, units = "px", scale = 3)
+          # show(bar.p)
           
         }
       }
@@ -211,48 +266,15 @@ tax_plot <- function(ps, top_x, group_variables,levels,
 }
 
 # top_xs <- c(10, 20, 40, "all")
-# groups <- c("Lake", "Mesh", "Station_ID")
-# ranks <- c("Species", "Genus", "Family", "Order", "Class")
-# tax_subsets <- c("unfiltered", "noRotifer", "noBenthic", "noRotifer_noBenthic")
+# groups <- c("Lake", "Mesh")
+# ranks <- c("Species", "Genus", "Family", "Order", "Class", "Phylum", "Kingdom)
+# tax_subsets <- c("unfiltered", "noRotifer", "noBenthic", "noRotifer_noBenthic", "onlyRotifer", "onlyBenthic")
 
-top_xs <- c(10)
+top_xs <- c(10, 20, 40, "all")
 groups <- c("Mesh")
-ranks <- c("Family")
-tax_subsets <- c("unfiltered")
+ranks <- c("ASV","Species","Genus", "Family", "Order", "Class")
+tax_subsets <- c("unfiltered", "noBenthic", "noRotifer", "noRotifer_noBenthic")
 
 
 tax_plot(ps, top_x = top_xs, group_variables = groups, levels = ranks,
         marker_outdir = markerdir, marker = marker, tax_subset = tax_subsets)
-  
-
-  
-  
-  
-  
-paste0("haha", top)
-  
-tax_subset <- "unfiltered"
-group_var <- "Lake"
-level <- "Species"
-top <- 20
-topchar <- as.character(top)
-  
-outfilepath <- paste0(markerdir, "/", "unfiltered", "/")
-
-outfilepath1 <- paste0(outfilepath, "Species", "/top", topchar, "/")
-dir.create(outfilepath1, recursive = TRUE)
-
-outfilebase <- paste0(outfilepath1, marker, "_", level, "_", tax_subset)
-outfilename <- paste0(outfilebase, "_", level, "_top", top, "_by", group_var,".png")
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
