e10aa37225fe11783da5a6e6e5e9d24d3c8085c3
Nitin Vincent <nitiowo98@gmail.com>
2026-01-18 21:17:14 -0500

    adding ASV job script

---
+++ 03_dadaASV/dada_asv_scripts/dada-bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -M nvincen2@nd.edu
+#$ -m abe
+#$ -q largemem
+#$ -pe smp 8
+#$ -N dada2_mICOint
+
+conda activate dada2-env
+
+Rscript /temp180/mpfrende/nvincen2/Metagenomics/03_dadaASV/dada_asv_scripts/dada2_asv.R\
+	--input /temp180/mpfrende/nvincen2/Metagenomics/02_cutadapt/demux_out/02c_rescue/mICOintF \
+	--output /temp180/mpfrende/nvincen2/Metagenomics/03_dadaASV/dada_asv_output/mICOintF \
+	--outprefix "mICOintF"
