c3160e3a0e2acfcdd49cdfca355037c3b99b6f74
Nitin Vincent <nvincen2@crcfe01.crc.nd.edu>
2026-01-27 18:26:24 -0500

    Changing output paths for QC scripts, and fixing conda environment

--- 01_QC/QC_scripts/fastQC.sh
+++ 01_QC/QC_scripts/fastQC.sh
@@ -4,13 +4,13 @@
 # conda activate fastqc-env
 
 # Create output directory if it doesn't exist
-mkdir -p ../fastqc_out
+mkdir -p ../fastqc_output
 
 # Finding raw data
 RAW_DIR=$(cat ../../00_prep/raw_dir.txt )
 
 # Run fastqc on every file in raw data directory
-fastqc "$RAW_DIR"/* -o ../fastqc_out/
+fastqc "$RAW_DIR"/* -o ../fastqc_output/
 
 # Deactivate fastqc
 # conda deactivate if necessary
--- 01_QC/QC_scripts/multiqc.sh
+++ 01_QC/QC_scripts/multiqc.sh
@@ -4,10 +4,10 @@
 #conda activate fastqc-env
 
 # Create output directory if it doesn't exist
-mkdir -p ../multiqc_out
+mkdir -p ../multiqc_output
 
 # Run multiqc on fastqc output
-multiqc ../fastqc_out/ -o ../multiqc_out
+multiqc ../fastqc_output/ -o ../multiqc_output
 
 # Deactivate multiqc
 #conda deactivate if necessary
