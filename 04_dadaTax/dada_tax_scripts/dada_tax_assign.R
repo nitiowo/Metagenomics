cb6760b3b074a845c1d3b89651e399ac023a3361
Nitin Vincent <nitiowo98@gmail.com>
2026-02-08 23:51:20 -0500

    Create dir and fix merge in data_tax_assign.R

--- 04_dadaTax/dada_tax_scripts/dada_tax_assign.R
+++ 04_dadaTax/dada_tax_scripts/dada_tax_assign.R
@@ -1,6 +1,9 @@
 library(dada2); packageVersion("dada2")
 library(argparser, quietly=TRUE)
 
+# Usage: dada_tax_assign.R --RDS --refFile --spref --outdir --outprefix
+
+##### SCRIPT MODE #####
 p <- arg_parser("DADA2 in R")
 
 # Defining arguments
@@ -10,15 +13,28 @@ p = add_argument(p, "--spref", help="Enter addSpecies ref file", type='character
 p = add_argument(p, "--outdir", help="Enter output directory", type='character')
 p = add_argument(p, "--outprefix", help = "Enter output prefix for filenames", type = 'character')
 
-
 argv <- parse_args(p)
 
-# Extracting argument values
+# Extracting argument values (Script mode)
 RDS_path = argv$RDS
 spref = argv$spref
 refDB = argv$refFile
 outdir = argv$outdir
 outprefix = paste0(outdir, "/", argv$outprefix, "_")
+
+#####
+
+##### INTERACTIVE MODE #####
+# RDS_path <-  "/Volumes/Samsung_1TB/Zooplankton/Metagenomics/03_dadaASV/dada_asv_output/mICOintF/mICOintF_seqtab.nochim.Rds"
+# spref <- "/Volumes/Samsung_1TB/Zooplankton/Metagenomics/DBs/final_dada_formatted/id_cluster_noempty_newparse.clustered.fasta"
+# refDB <- "/Volumes/Samsung_1TB/Zooplankton/Metagenomics/DBs/final_dada_formatted/tax_cluster_noempty_newparse.clustered.fasta"
+# outdir <- "/Volumes/Samsung_1TB/Zooplankton/Metagenomics/04_dadaTax/dada_tax_output/mICOintF1"
+# outprefix <- "mICOintF"
+# outprefix <- paste0(outdir, "/", outprefix, "_")
+
+#####
+
+dir.create(outdir)
 
 # Load in files
 print("Loading files")
@@ -27,18 +43,18 @@ otutab <- t(seqtab.nochim)
 
 # Assign taxonomy
 print("assignTaxonomy")
-taxa <- assignTaxonomy(seqtab.nochim, refDB, multithread=TRUE, tryRC=TRUE)
+taxa <- assignTaxonomy(seqtab.nochim, refDB, multithread = TRUE, tryRC = TRUE)
 
 print("addSpecies")
 taxa <- addSpecies(taxa, spref)
 
 # Writing outputs
 print("Writing outputs")
-write.table(taxa, file = paste0(outprefix,"taxa.tsv"), quote=FALSE)
-saveRDS(taxa, paste0(outprefix,"taxa.Rds"))
+write.table(taxa, file = paste0(outprefix, "taxa.tsv"), quote = FALSE)
+saveRDS(taxa, paste0(outprefix, "taxa.Rds"))
 
 # Create abundance table with tax assignments and counts - merged by ASVs (rownames)
-abundance <- merge(taxa, otutab, by = "row.names", all - TRUE)
+abundance <- merge(taxa, otutab, by = "row.names", all = TRUE)
 write.csv(abundance, file = paste0(outprefix, "abundance.csv"), quote = FALSE)
 
 # The following line replaces any instances of NNNNNNNNNN with nothing
